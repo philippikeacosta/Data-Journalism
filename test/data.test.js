c7097d9cbf32e5d55d83b285390d4fb31829bbfc
philippikeacosta <philip.pike-acosta21@trinityschoolnyc.org>
2020-12-11 14:54:08 -0500

    Frances' Edits

---
+++ test/data.test.js
@@ -0,0 +1,38 @@
+let lifeExpect = require('../data/clean/life.json');
+let election = require('../data/clean/election.json');
+
+describe('checking data format', () => {
+  test("All life expectancies should be represented as decimals", () =>{
+
+  });
+  test("All % should be represented as decimals", () =>{
+
+  });
+  test("All election result data should be represented by a D or R", () =>{
+
+  });
+  test("All state names should just be the name of the state", () =>{
+
+  });
+  test("No duplicate states in either data set", () =>{
+
+  });
+});
+
+describe('checking data completeness', () => {
+  test("All states represented in life expectancy data", () =>{
+
+  });
+  test("All states represented in election data", () =>{
+
+  });
+  test("All states should have election result data", () =>{
+
+  });
+  test("All states should have election vote data for democratic, republican, and other", () =>{
+
+  });
+  test("All states should have election percentage data for democratic, republican, and other", () =>{
+
+  });
+});
