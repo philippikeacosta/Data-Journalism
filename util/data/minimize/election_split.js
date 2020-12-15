54dc8106b73adc0b21dfac363d33803c066a4d0d
philippikeacosta <philip.pike-acosta21@trinityschoolnyc.org>
2020-12-15 12:06:18 -0500

    deleted useless comments

--- util/data/minimize/election_split.js
+++ util/data/minimize/election_split.js
@@ -6,7 +6,7 @@ let election_csv = fs.readFileSync('../../../data/original/election.csv', 'utf8'
 
 let election = election_csv.split("\n");
 
-for(let i = 0; i<election.length-1; i++){
+for (let i = 0; i < election.length - 1; i++) {
   let state_info = election[i].split(',');
   let state = {};
   state['state'] = state_info[0];
@@ -20,19 +20,6 @@ for(let i = 0; i<election.length-1; i++){
 
   states.push(state);
 }
-// election.forEach(function(election) {
-//   let state_info = election.split(',');
-//   let state = {};
-//   state['state'] = state_info[0];
-//   state['result'] = state_info[1];
-//   state['Dvotes'] = state_info[3];
-//   state['Rvotes'] = state_info[4];
-//   state['Ovotes'] = state_info[5];
-//   state['Dpercent'] = state_info[6];
-//   state['Rpercent'] = state_info[7];
-//   state['Opercent'] = state_info[8];
-//
-//   states.push(state);
-// });
+
 
 fs.writeFileSync('../../../data/clean/election.json', JSON.stringify(states), 'utf8');
