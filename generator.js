4f1f92bf163b5e65dc420b3a0147b71836b813c0
philippikeacosta <philip.pike-acosta21@trinityschoolnyc.org>
2020-12-15 11:49:46 -0500

    Landing Page

--- generator.js
+++ generator.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const ejs = require('ejs');
+let election = JSON.parse(fs.readFileSync('data/clean/election.json', 'utf8'));
+let life = JSON.parse(fs.readFileSync('data/clean/life.json', 'utf8'));
 
 //about
 let about_template = fs.readFileSync('views/about.ejs', 'utf8');
@@ -8,3 +10,10 @@ let about_html = ejs.render(about_template, {
   title: "About",
 });
 fs.writeFileSync("build/about.html", about_html, 'utf8');
+
+let landing_template = fs.readFileSync('views/landing.ejs', 'utf8');
+let landing_html = ejs.render(landing_template, {
+  filename: __dirname + '/views/landing.ejs',
+  title: "Home",
+});
+fs.writeFileSync("build/landing.html", landing_html, 'utf8');
