119c02c81e47e5cf5f9637bf76a67bd44cbb81e0
philippikeacosta <philip.pike-acosta21@trinityschoolnyc.org>
2020-12-15 11:57:21 -0500

    For Each is Stupid

--- util/data/minimize/life_split.js
+++ util/data/minimize/life_split.js
@@ -7,14 +7,25 @@ let life_csv = fs.readFileSync('../../../data/original/life.csv', 'utf8');
 let life = life_csv.split("\n");
 
 
-life.forEach(function(life) {
-  let state_info = life.split(',');
+for(let i = 0; i<life.length-1; i++){
+  let state_info = life[i].split(',');
   let state = {};
   state['state'] = state_info[0];
   state['avg'] = state_info[1];
 
 
   states.push(state);
-});
+
+}
+
+// life.forEach(function(life) {
+//   let state_info = life.split(',');
+//   let state = {};
+//   state['state'] = state_info[0];
+//   state['avg'] = state_info[1];
+//
+//
+//   states.push(state);
+// });
 
 fs.writeFileSync('../../../data/clean/life.json', JSON.stringify(states), 'utf8');
