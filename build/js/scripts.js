0ab87364297bfddf57368e8da5b0c9fc6a80653d
philippikeacosta <philip.pike-acosta21@trinityschoolnyc.org>
2021-01-04 18:12:42 -0500

    Added plotly map

---
+++ build/js/scripts.js
@@ -0,0 +1,57 @@
+var test = document.getElementById("test");
+
+// let election = require('../data/clean/election.json');
+//
+console.log(Object.values(election[0])[5]);
+
+
+var data = [{
+  type: "choroplethmapbox",
+  name: "US states",
+  geojson: "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json",
+  locations: ["AZ","FL","GA", "IA","MI","MN","NV", "NH", "NC","OH", "PA", "TX","WI", "AL", "AK",  "AR", "CA", "CO", "CT", "DE", "DC",  "HI", "ID", "IL", "IN", "KS", "KY", "LA", "ME", "MD", "MA", "MS", "MO", "MT", "NE",  "NJ", "NM", "NY",  "ND", "OK", "OR", "RI", "SC", "SD", "TN",  "UT", "VT", "VA", "WA", "WV", "WY"],
+  z: [141, 140, 155, 147, 132, 146, 151, 137, 146, 136, 145, 141, 149, 151, 138, 158, 164, 141, 146, 145, 142, 150, 155, 160, 156, 161, 147, 164, 150, 152, 155, 167, 145, 146, 151, 154, 161, 145, 155, 150, 151, 162, 172, 169, 170, 151, 152, 173, 160, 176],
+  zmin: 0.27,
+  zmax: 0.66,
+  colorscale: [[0, 'rgb(255,0,0)'], [1, 'rgb(0,0,255)']],
+  colorbar: {
+    y: 0,
+    yanchor: "bottom",
+    title: {
+      text: "Biden Vote Share",
+      side: "right"
+    }
+  }
+}];
+
+console.log( Object.values(data)[0].locations);
+
+
+for(let i = 0; i < 51; i++){
+  //Object.values(data)[0].locations[i] = Object.values(election[i+1])[0];
+  Object.values(data)[0].z[i] = Object.values(election[i+1])[5];
+}
+console.log(Object.values(data));
+
+var layout = {
+  mapbox: {
+    style: "dark",
+    center: {
+      lon: -96,
+      lat: 40
+    },
+    zoom: 3.5
+  },
+  width: 1200,
+  height: 800,
+  margin: {
+    t: 0,
+    b: 0
+  }
+};
+
+var config = {
+  mapboxAccessToken: "pk.eyJ1IjoicHBpa2VhIiwiYSI6ImNramV4czZqbzV0ZXcycXJ3OG0wNDBvc3cifQ.MAj1mFEKGNJffhkPrB7J9A"
+};
+
+Plotly.newPlot(test, data, layout, config);
--- util/generator.js
+++ util/generator.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const ejs = require('ejs');
-let election = JSON.parse(fs.readFileSync('data/clean/election.json', 'utf8'));
-let life = JSON.parse(fs.readFileSync('data/clean/life.json', 'utf8'));
+let election = JSON.parse(fs.readFileSync('../data/clean/election.json', 'utf8'));
+let life = JSON.parse(fs.readFileSync('../data/clean/life.json', 'utf8'));
 
 //about
-let about_template = fs.readFileSync('views/about.ejs', 'utf8');
+let about_template = fs.readFileSync('../views/about.ejs', 'utf8');
 let about_html = ejs.render(about_template, {
   filename: __dirname + '/../views/about.ejs',
   title: "About",
 });
-fs.writeFileSync("build/about.html", about_html, 'utf8');
+fs.writeFileSync("../build/about.html", about_html, 'utf8');
 
-let landing_template = fs.readFileSync('views/landing.ejs', 'utf8');
+let landing_template = fs.readFileSync('../views/landing.ejs', 'utf8');
 let landing_html = ejs.render(landing_template, {
   filename: __dirname + '/../views/landing.ejs',
   title: "Home",
 });
-fs.writeFileSync("build/landing.html", landing_html, 'utf8');
+fs.writeFileSync("../build/landing.html", landing_html, 'utf8');
