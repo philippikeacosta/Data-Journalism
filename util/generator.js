7e11429e24001247f4ea9a8f4aa2a8539cf25cbf
philippikeacosta <philip.pike-acosta21@trinityschoolnyc.org>
2021-01-16 18:07:49 -0500

    Micro Pages!

--- util/generator.js
+++ util/generator.js
@@ -5,17 +5,32 @@ let life = JSON.parse(fs.readFileSync('./data/clean/life.json', 'utf8'));
 let filename = "";
 
 
-election.forEach(function(state){
+// election.forEach(function(state){
+//   let state_template = fs.readFileSync('./views/micro.ejs', 'utf8');
+//   filename = "./build/html/" + state.state.replace(/ /g, "_").replace("?", "_") + ".html";
+//   let state_html = ejs.render(state_template, {
+//     filename: __dirname + '/../views/micro.ejs',
+//     data: state,
+//     election: JSON.stringify(election),
+//     life: JSON.stringify(life),
+//   });
+//   fs.writeFileSync(filename, state_html, 'utf8');
+// });
+
+for(let i = 0; i<51; i++){
   let state_template = fs.readFileSync('./views/micro.ejs', 'utf8');
-  filename = "./build/html/" + state.state.replace(/ /g, "_").replace("?", "_") + ".html";
+  filename = "./build/html/" + life[i].state.replace(/ /g, "_").replace("?", "_") + ".html";
   let state_html = ejs.render(state_template, {
     filename: __dirname + '/../views/micro.ejs',
-    data: state,
-    election: election,
-    life: life,
+    data: election[i+1],
+    data2: life[i],
+    election: JSON.stringify(election[i+1]),
+    life: JSON.stringify(life[i]),
+    Dpercent: election[i+1].Dpercent * 100,
+    Rpercent: election[i+1].Rpercent * 100,
   });
   fs.writeFileSync(filename, state_html, 'utf8');
-});
+}
 
 //about
 let about_template = fs.readFileSync('./views/about.ejs', 'utf8');
